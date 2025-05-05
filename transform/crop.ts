ec895ee6a6fecba448f4f4c47e55c099ee3f0972
György Kovács <pktzerus@gmail.com>
2025-05-05 05:28:02 +0200

    Additional region helpers

--- transform/crop.ts
+++ transform/crop.ts
@@ -1,10 +1,140 @@
 import { createCanvas, ImageData } from 'canvas';
 
+/** A rectangle specified by its top left coordinates and width / height */
 export interface Region {
     x: number;
     y: number;
     width: number;
     height: number;
+}
+
+/** Create a region from top left (inclusive) and bottom right (exclusive) coordinates */
+export function regionFromBounds(
+    xFrom: number, xTo: number,
+    yFrom: number, yTo: number,
+): Region {
+    return {
+        x: xFrom,
+        y: yFrom,
+        width: xTo - xFrom,
+        height: yTo - yFrom,
+    };
+}
+
+/**
+ * Creates the regions between the given horizontal and vertical lines, leaving
+ * gaps between each subsequent pair of lines.
+ *
+ * For example, given the lines below:
+ *
+ * H: [1, 2, 3, 4]; V: [5, 6, 7, 8]
+ *
+ * The result would be the following regions (horizontal / vertical boundaries):
+ *
+ * - (1-2, 5-6)
+ * - (1-2, 7-8)
+ * - (3-4, 5-6)
+ * - (3-4, 7-8)
+ */
+export function regionsBetweenLines(horizontal: number[], vertical: number[]): Region[] {
+    if (horizontal.length % 2 !== 0 || vertical.length % 2 !== 0) {
+        throw RangeError("Length of both input arrays must be even.");
+    }
+
+    const result: Region[] = [];
+    for (let h = 0; h < horizontal.length / 2; h += 1) {
+        for (let v = 0; v < vertical.length / 2; v += 1) {
+            result.push(regionFromBounds(
+                vertical[v * 2], vertical[v * 2 + 1],
+                horizontal[h * 2], horizontal[h * 2 + 1],
+            ));
+        }
+    }
+
+    return result;
+}
+
+/**
+ * Creates the regions between the given horizontal and vertical lines, where
+ * each line is a shared boundary between the two neighboring regions.
+ *
+ * For example, given the lines below:
+ *
+ * H: [1, 2, 3]; V: [5, 6, 7]
+ *
+ * The result would be the following regions (horizontal / vertical boundaries):
+ *
+ * - (1-2, 5-6)
+ * - (1-2, 6-7)
+ * - (2-3, 5-6)
+ * - (2-3, 6-7)
+ */
+export function regionsBetweenSharedLines(horizontal: number[], vertical: number[]): Region[] {
+    if (horizontal.length <= 2 || vertical.length <= 2) {
+        throw RangeError("Both input arrays must contain at least 2 items.");
+    }
+
+    const result: Region[] = [];
+    for (let h = 0; h < horizontal.length - 1; h += 1) {
+        for (let v = 0; v < vertical.length - 1; v += 1) {
+            result.push(regionFromBounds(
+                vertical[v], vertical[v + 1],
+                horizontal[h], horizontal[h + 1],
+            ));
+        }
+    }
+
+    return result;
+}
+
+/** Extends the region outwards by an equal amount in each direction */
+export function padRegion(original: Region, padding: number): Region {
+    return {
+        x:      original.x - padding,
+        y:      original.y - padding,
+        width:  original.width + 2 * padding,
+        height: original.height + 2 * padding,
+    };
+}
+
+/** Returns a region covering the two inputs */
+export function mergeRegions(r1: Region, r2: Region): Region {
+    const xFrom = Math.min(r1.x, r2.x);
+    const yFrom = Math.min(r1.y, r2.y);
+    const xEnd = Math.max(r1.x + r1.width, r2.x + r2.width);
+    const yEnd = Math.max(r1.y + r1.height, r2.y + r2.height);
+
+    return {
+        x: xFrom,
+        y: yFrom,
+        width: xEnd - xFrom,
+        height: yEnd - yFrom,
+    };
+}
+
+/**
+ * Generates regions starting from the top left one, and repeated for the number
+ * of row / columns, with the given horizontal / vertical gaps in-between each
+ * neighboring cell.
+ */
+export function regionGrid(
+    initial: Region,
+    gapX: number, gapY: number,
+    columns: number, rows: number,
+): Region[] {
+    const result: Region[] = [];
+
+    for (let r = 0; r < rows; r += 1) {
+        for (let c = 0; c < columns; c += 1) {
+            result.push({
+                x: initial.x + c * (initial.width + gapX),
+                y: initial.y + r * (initial.height + gapY),
+                width: initial.width,
+                height: initial.height,
+            });
+        }
+    }
+    return result;
 }
 
 /** Extracts one section of the input image, defined by the given region. */
@@ -58,4 +188,3 @@ export function scaleTo(source: ImageData, targetWidth: number, targetHeight: nu
     outCtx.drawImage(srcCanvas, 0, 0, newWidth, newHeight);
     return outCtx.getImageData(0, 0, newWidth, newHeight);
 }
-
